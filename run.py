0c15c36f8189a751c95c3ff52fd61c9f0ca5f40d
ania15 <annakaniowska7@gmail.com>
2024-04-12 15:42:03 +0200

    SPI calculations, documentation, structure correction

--- run.py
+++ run.py
@@ -8,12 +8,14 @@ from src.visualizations.visualize_timeseries_data import visualize_available_voi
 from src.preprocessing.preprocessing_stations import get_and_save_voi_missing_stations
 from src.preprocessing.clipping import clip_data_to_voi
 from src.preprocessing.preprocessing_precip import preprocess_precipitation
-from src.preprocessing.obtaining_basic_statistics import get_basic_statistics
+from src.calculations.obtain_basic_statistics import get_basic_statistics
 from src.utils.utils import save_df
 from src.visualizations.visualize_EDA_results import visualize_EDA
+from src.calculations.calculate_SPI import get_SPI
 import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning)
+
 
 def main(voi):
     # Data acquisition
@@ -23,16 +25,22 @@ def main(voi):
     voi_polygon, voi_precip, voi_stations = clip_data_to_voi(all_precip, stations, voi)
     get_and_save_voi_missing_stations(all_precip, stations, voi)
     preprocessed_df = preprocess_precipitation(voi_precip, voi)
+
     # After ALL preprocessing is done - save the precip file (not earlier!)
     save_df(preprocessed_df, f"preprocessed_{voi}_data.csv")
+
+    # Obtaining basic statistics for preprocessed data
     get_basic_statistics(preprocessed_df)
     
     # Visualizations
     visualize_stations(voi_polygon, voi_stations, voi)
     visualize_available_voi_data(voi_precip, voi)
 
-    # EDA visualizations
+    # EDA visualizations for precipitation data
     visualize_EDA(preprocessed_df, voi)
+
+    # SPI calculations for precipitation data
+    SPI_1, SPI_3, SPI_12 = get_SPI(preprocessed_df)
 
 
 if __name__ == "__main__":
---
+++ src/calculations/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["obtain_basic_statistics.py", "calculate_SPI.py"]
